66d7ac7dcd10d0207902b4c3efe4dbbb3141dfe4
TrucLinh <miner337t@gmail.com>
2026-03-12 00:12:13 +0700

    update web app

---
+++ frontend/src/pages/Notifications/Notifications.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Notifications() {
+  return (
+    <div>
+      <h1>Notifications</h1>
+      <p>Welcome to the Notifications page!</p>
+    </div>
+  );
+}
