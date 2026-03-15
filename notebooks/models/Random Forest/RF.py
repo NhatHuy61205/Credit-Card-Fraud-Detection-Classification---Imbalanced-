fe85ea4027281250d1b01492c80e0813bee55324
TrucLinh <miner337t@gmail.com>
2026-03-15 17:28:54 +0700

    update wandb

--- notebooks/models/Random Forest/RF.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, roc_auc_score
-from pathlib import Path
-import pandas as pd
-
-path = Path("data/creditcard.csv")
-
